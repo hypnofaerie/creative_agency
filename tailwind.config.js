0e212d0afe58fb57e95cb50222e0b802a045f642
hypnofaerie <102352746+hypnofaerie@users.noreply.github.com>
2023-01-12 15:45:05 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                violet: {
+                    950: '#28222f',  // dark bg
+                },
+            }
+        },
+    },
+}
